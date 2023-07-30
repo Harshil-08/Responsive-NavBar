dc5df6d24885dfbd1081afba877fb6592bee5f79
Harshil <harshiltanwar62@gmail.com>
2023-07-30 19:50:02 +0530

    background

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,7 @@
 const sideBar = document.getElementById("sideBar");
-// sideBar.style.backgroundColor = "red";
-
 const hamburgerBtn = document.querySelector(".hamburger-btn");
 
 hamburgerBtn.addEventListener("click", () => {
-    hamburgerBtn.classList.toggle("active");
-    sideBar.classList.toggle("hidden");
+  hamburgerBtn.classList.toggle("active");
+  sideBar.classList.toggle("hidden");
 });
